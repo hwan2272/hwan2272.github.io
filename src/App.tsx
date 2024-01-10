9880b4ac299ac5c878721262e19130e831cb9d2c
hwan2272 <hwan2230@naver.com>
2024-01-10 23:22:12 +0900

    포트폴리오 작업
    1. deploy 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
-import MyInfo from "./pages/Contents/MyInfo";
-import Welcome from "./pages/Contents/Welcome";
-import Contact from "./pages/Contents/Contact";
-import Projects from "./pages/Contents/Projects";
+// import MyInfo from "./pages/Contents/MyInfo";
+// import Welcome from "./pages/Contents/Welcome";
+// import Contact from "./pages/Contents/Contact";
+// import Projects from "./pages/Contents/Projects";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
-import LeftMenu from "./pages/LeftMenu";
+// import LeftMenu from "./pages/LeftMenu";
 import Contents from "./pages/Contents/Contents";
 
 function App() {
